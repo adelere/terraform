bd42336ed8441283ad3015fd2be6608163acde33
adelere <70536431+adelere@users.noreply.github.com>
2024-06-26 18:32:31 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,11 +3,11 @@ provider "azurerm" {
   environment = "usgovernment"
 }
 
-resource "azurerm_resource_group" "example" {
-  name     = "example-resource-group"
+resource "azurerm_resource_group" "testgrp" {
+  name     = "testgrp-resource-group"
   location = "US Gov Virginia"
 }
 
 output "resource_group_name" {
-  value = azurerm_resource_group.example.name
+  value = azurerm_resource_group.testgrp.name
 }
